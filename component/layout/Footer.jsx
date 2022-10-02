3eb4e5433e2b4d8dacb0c1a7fcf2c71f6908677a
raoufgr <raouf.grera@gmail.com>
2022-10-02 08:24:15 +0200

    emal

--- component/layout/Footer.jsx
+++ component/layout/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <>
             <footer className='footer'>
-                <div>+218927223001 | info@leocodes.com</div>
+                <div>+218927223001 | raouf.grera@gmail.com</div>
 
             </footer>
         </>
